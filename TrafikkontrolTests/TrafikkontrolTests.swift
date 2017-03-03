9e08ef05635cedb350cf1bbf5079fcc2850136ee
Jonas Simonsen <jonassimonsen@Jonass-MacBook-Pro.local>
2017-03-03 08:43:34 +0100

    Initial Commit

---
+++ TrafikkontrolTests/TrafikkontrolTests.swift
@@ -0,0 +1,36 @@
+//
+//  TrafikkontrolTests.swift
+//  TrafikkontrolTests
+//
+//  Created by Jonas Simonsen on 03/03/2017.
+//  Copyright © 2017 Jonas Simonsen. All rights reserved.
+//
+
+import XCTest
+@testable import Trafikkontrol
+
+class TrafikkontrolTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
